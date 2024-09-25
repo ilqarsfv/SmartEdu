0bc15c2e4674bd5b638404adbc1d71f00f26dcd8
ilqarsfv <ilqar.sofiyev.95@gmail.com>
2024-09-25 15:00:07 +0400

    second commite

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require("express");
 
 const app = express();
 
+// Template Engin
+app.set("view engine","ejs")
+
+// middlewares
+app.use(express.static("public"))
+
+// routes
 app.get("/", (req, res) => {
-  res.send("index");
+  res.render("index",{
+    page_name: "index"
+  });
+});
+app.get("/about", (req, res) => {
+  res.render("about",{
+    page_name: "about"
+  });
 });
 
 const port = 2000;
